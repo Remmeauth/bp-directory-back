1e0cd0efbddf1cffc76a7197e841a3514cf19ea1
Dmytro Striletskyi <dmytro.striletskyi@gmail.com>
2019-07-16 17:57:42 +0300

    Fetch constants from envs, rename project, wait until postgres is started

--- ops/development/start-server.sh
+++ ops/development/start-server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-python directory/manage.py migrate
+source $(dirname $0)/database-utils.sh
+
+wait_until_postgres_is_started && \
+    python directory/manage.py migrate
 
 python directory/manage.py runserver 0.0.0.0:8000
