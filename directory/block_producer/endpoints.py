2cbc3ed4e1373351e04a68094b497954eaaca2f7
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-07-24 22:09:08 +0300

    Implement block producer card

--- directory/block_producer/endpoints.py
+++ directory/block_producer/endpoints.py
@@ -3,8 +3,10 @@ Provide implementation of block producer endpoints.
 """
 from django.urls import path
 
+from block_producer.views.block_producer import BlockProducerSingle
 from block_producer.views.like import BlockProducerLikeSingle
 
 block_producer_endpoints = [
+    path('', BlockProducerSingle.as_view()),
     path('<int:block_producer_id>/like/', BlockProducerLikeSingle.as_view()),
 ]
--- directory/block_producer/forms.py
+++ directory/block_producer/forms.py
@@ -1,3 +1,28 @@
 """
 Provide implementation of operation with block producer forms.
 """
+from django import forms
+
+
+class CreateBlockProducerForm(forms.Form):
+    """
+    Create block producer form implementation.
+    """
+
+    name = forms.CharField(required=True)
+    website_url = forms.URLField(required=True)
+    location = forms.CharField(required=False)
+    short_description = forms.CharField(required=True)
+    full_description = forms.CharField(widget=forms.Textarea, required=False)
+    logo_url = forms.URLField(required=False)
+
+    linkedin_url = forms.URLField(required=False)
+    twitter_url = forms.URLField(required=False)
+    medium_url = forms.URLField(required=False)
+    github_url = forms.URLField(required=False)
+    facebook_url = forms.URLField(required=False)
+    telegram_url = forms.URLField(required=False)
+    reddit_url = forms.URLField(required=False)
+    slack_url = forms.URLField(required=False)
+    wikipedia_url = forms.URLField(required=False)
+    steemit_url = forms.URLField(required=False)
