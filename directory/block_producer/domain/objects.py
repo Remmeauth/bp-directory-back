d99ad67630a02b686a7299769c8ef379584e5987
Anastasiia Bilova <anastasiabilova@gmail.com>
2019-09-29 21:34:57 +0300

    Create domain object for rejected block producer description

--- directory/block_producer/domain/objects.py
+++ directory/block_producer/domain/objects.py
@@ -298,9 +298,9 @@ class DeleteBlockProducer:
         return self.block_producer.delete_(identifier=block_producer_id)
 
 
-class RejectedBlockProducer:
+class RejectedBlockProducerDescription:
     """
-    Sent block producer status description to the email address implementation.
+    Send block producer status description to the email address implementation.
     """
 
     def __init__(self, user, block_producer):
@@ -312,7 +312,7 @@ class RejectedBlockProducer:
 
     def do(self, email, block_producer_id):
         """
-        Sent block producer status description to the email address.
+        Send block producer status description to the email address.
         """
         if not self.user.does_exist_by_email(email=email):
             raise UserWithSpecifiedEmailAddressDoesNotExistError
